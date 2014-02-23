369ab2612d661d7f7f522016928feaa5051c55fa
Emil Janitzek <emil@pixel2.se>
2014-02-24 00:33:54 +0100

    Updated to follow api 1.0 spec

--- tinypngapi.php
+++ tinypngapi.php
@@ -1,57 +1,58 @@
 <?php
-require_once("apisettings.php");
-
 class TinyPngApi
-{ 
+{
+  private function getLocation($response, $curl) 
+  {
+    $headers = substr($response, 0, curl_getinfo($curl, CURLINFO_HEADER_SIZE));
+    foreach (explode("\r\n", $headers) as $header) {
+      if (substr($header, 0, 10) === "Location: ") {
+        return substr($header, 10);
+      }
+    }
+  }
+
+  private function downloadTinyPng($url, $tmpImagePath)
+  {
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => $url,
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_SSL_VERIFYPEER => true
+    ));
+
+    return file_put_contents($tmpImagePath, curl_exec($curl));
+  }
+
   public function getTinyPng($image)
   {
-    global $tinyPngApiKey;
+    $tinyPngApiKey = getenv('tinyPngApiKey');
+
     $curl = curl_init();
-    
-    curl_setopt($curl, CURLOPT_URL, "http://api.tinypng.org/api/shrink");
-    curl_setopt($curl, CURLOPT_USERPWD, "api:" . $tinyPngApiKey);
-    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_POST, true);
-    curl_setopt($curl, CURLOPT_BINARYTRANSFER, true);
-    curl_setopt($curl, CURLOPT_POSTFIELDS, file_get_contents($image));
-      
+
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => "https://api.tinypng.com/shrink",
+        CURLOPT_USERPWD => "api:" . $tinyPngApiKey,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_BINARYTRANSFER => true,
+        CURLOPT_SSL_VERIFYPEER => true,
+        CURLOPT_POST => true,
+        CURLOPT_HEADER => true,
+        CURLOPT_POSTFIELDS => file_get_contents($image)));
+
     $tmpImagePath = sprintf("pictures/tmp/%s-tiny.png", time() * rand(2,10));
-    $result = curl_exec($curl);
-    $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-    curl_close($curl);
-    
-    if ($result && $httpCode == 200)
-    { 
-      $json = json_decode($result, true);
-      if ($this->downloadTinyPng($json["output"]["url"], $tmpImagePath))
+
+    $response = curl_exec($curl);
+    var_dump($response);
+    if (curl_getinfo($curl, CURLINFO_HTTP_CODE) === 201)
+    {
+      if ($this->downloadTinyPng($this->getLocation($response, $curl), $tmpImagePath))
+      {
         return $tmpImagePath;
+      }
     }
-   
+    
     return $image;
-  }
-  
-  public function downloadTinyPng($url, $tmpImagePath)
-  {
-    global $tinyPngApiKey;
-    $curl = curl_init();
-    
-    curl_setopt($curl, URLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_USERPWD, "api:" . $tinyPngApiKey);
-    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    
-    $result = curl_exec($curl);
-    $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-    curl_close($curl);
-    
-    if ($result && $httpCode == 200)
-    { 
-      file_put_contents($tmpImagePath, $result);
-      return true;
-    }
-    
-    return false;
   }
 }
 ?>
